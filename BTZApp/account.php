30af8d0e6ca8bba05c1e12c3b9d8db17adaddb34
Brian Treichel <Trechy@users.noreply.github.com>
2018-04-10 11:05:45 -0400

    Sign In/Up CSS
    Fixed the styling for the new sign in/up page we're using

--- BTZApp/account.php
+++ BTZApp/account.php
@@ -37,16 +37,16 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <div id="nav-bar">
         <ul class="navbar">
             <li class="navTitle"><a href="index.php">Real Est8</a></li>
-            <li id="account-dropdown" class="dropdown">
-                <a href="account.php" class="dropbtn">Sign in</a>
-            </li>
+<!--            <li id="account-dropdown" class="dropdown">-->
+<!--                <a href="account.php" class="dropbtn">Sign in</a>-->
+<!--            </li>-->
         </ul>
     </div>
     <div class="form">
 
         <ul class="tab-group">
-            <li class="tab"><a href="#signup">Sign Up</a></li>
             <li class="tab active"><a href="#login">Log In</a></li>
+            <li class="tab"><a href="#signup">Sign Up</a></li>
         </ul>
 
         <div class="tab-content">
@@ -58,14 +58,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
                     <div class="field-wrap">
                         <label>
-                            Email Address<span class="req">*</span>
+                            Email Address<span class="req"></span>
                         </label>
                         <input type="email" required autocomplete="off" name="email"/>
                     </div>
 
                     <div class="field-wrap">
                         <label>
-                            Password<span class="req">*</span>
+                            Password<span class="req"></span>
                         </label>
                         <input type="password" required autocomplete="off" name="password"/>
                     </div>
@@ -79,21 +79,21 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
             </div>
 
             <div id="signup">
-                <h1>Sign Up for Free</h1>
+                <h1>Sign Up</h1>
 
                 <form action="account.php" method="post" autocomplete="off">
 
                     <div class="top-row">
                         <div class="field-wrap">
                             <label>
-                                First Name<span class="req">*</span>
+                                First Name<span class="req"></span>
                             </label>
                             <input type="text" required autocomplete="off" name='firstname' />
                         </div>
 
                         <div class="field-wrap">
                             <label>
-                                Last Name<span class="req">*</span>
+                                Last Name<span class="req"></span>
                             </label>
                             <input type="text"required autocomplete="off" name='lastname' />
                         </div>
@@ -101,30 +101,37 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
                     <div class="field-wrap">
                         <label>
-                            Email Address<span class="req">*</span>
+                            Email Address<span class="req"></span>
                         </label>
                         <input type="email"required autocomplete="off" name='email' />
                     </div>
 
                     <div class="field-wrap">
                         <label>
-                            Set A Password<span class="req">*</span>
+                            Password<span class="req"></span>
                         </label>
                         <input type="password"required autocomplete="off" name='password'/>
                     </div>
 
+                    <div class="field-wrap">
+                        <label>
+                            Confirm Password<span class="req"></span>
+                        </label>
+                        <input type="password"required autocomplete="off" name='confirmPassword'/>
+                    </div>
+
+                    <div class="field-wrap">
+                        <label>
+                            Phone Number<span class="req"></span>
+                        </label>
+                        <input type="text" required autocomplete="off" name='phone'/>
+                    </div>
                     <button type="submit" class="button button-block" name="register" />Register</button>
-
                 </form>
-
             </div>
-
         </div><!-- tab-content -->
-
     </div> <!-- /form -->
     <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
-
     <script src="js/index.js"></script>
-
 </body>
 </html>
