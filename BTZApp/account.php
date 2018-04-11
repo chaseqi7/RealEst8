804363cc9f8a44a4b7b416001362a15cbf614ca7
KatrinaTingTingLin <31769438+KatrinaTingTingLin@users.noreply.github.com>
2018-04-11 19:42:30 -0400

    Remove spaces and update script

--- BTZApp/account.php
+++ BTZApp/account.php
@@ -15,24 +15,21 @@ session_start();
 <head>
     <title>Sign-Up/Login Form</title>
     <?php include 'css/css.html'; ?>
-<!--    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>-->
+<!--    <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>-->-->
 <!--    <script src="libraries/jquery.validate.js"></script>-->
 <!--    <script src="js/index.js"></script>-->
 <!--    <script src="js/validation.js"></script>-->
 </head>
+
 <?php
 if ($_SERVER['REQUEST_METHOD'] == 'POST')
 {
     if (isset($_POST['login'])) { //user logging in
-
         require 'login.php';
-
     }
-
-    elseif (isset($_POST['register'])) { //user registering
-
+    elseif (isset($_POST['register'])) {
+        //user registering
         require 'register.php';
-
     }
 }
 ?>
@@ -40,89 +37,71 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
     <div id="nav-bar">
         <ul class="navbar">
             <li class="navTitle"><a href="index.php">Real Est8</a></li>
-<!--            <li id="account-dropdown" class="dropdown">-->
-<!--                <a href="account.php" class="dropbtn">Sign in</a>-->
-<!--            </li>-->
+            <li id="account-dropdown" class="dropdown">
+                <a href="account.php" class="dropbtn">Sign in</a>
+            </li>
         </ul>
     </div>
     <div class="form">
-
         <ul class="tab-group">
             <li class="tab active"><a href="#login">Log In</a></li>
             <li class="tab"><a href="#signup">Sign Up</a></li>
         </ul>
-
         <div class="tab-content">
-
             <div id="login">
                 <h1>Welcome Back!</h1>
-
                 <form name="signInForm" action="account.php" method="post" autocomplete="off">
-
                     <div class="field-wrap">
                         <label>
-                            Email Address<span class="req"></span>
+                            Email Address<span class="req">*</span>
                         </label>
                         <input type="email" autocomplete="off" name="email"/>
                     </div>
-
                     <div class="field-wrap">
                         <label>
-                            Password<span class="req"></span>
+                            Password<span class="req">*</span>
                         </label>
                         <input type="password" autocomplete="off" name="password"/>
                     </div>
-
                     <p class="forgot"><a href="forgot.php">Forgot Password?</a></p>
-
                     <button class="button button-block" name="login" id="login"/>Log In</button>
-
                 </form>
-
             </div>
-
             <div id="signup">
                 <h1>Sign Up</h1>
-
                 <form name="signUpForm" action="account.php" method="post" autocomplete="off">
-
                     <div class="top-row">
                         <div class="field-wrap">
                             <label>
-                                First Name<span class="req"></span>
+                                First Name<span class="req">*</span>
                             </label>
                             <input type="text" autocomplete="off" name='firstname' />
                         </div>
-
                         <div class="field-wrap">
                             <label>
-                                Last Name<span class="req"></span>
+                                Last Name<span class="req">*</span>
                             </label>
                             <input type="text" autocomplete="off" name='lastname' />
                         </div>
                     </div>
-
                     <div class="field-wrap">
                         <label>
-                            Email Address<span class="req"></span>
+                            Email Address<span class="req">*</span>
                         </label>
                         <input type="email" autocomplete="off" name='email' />
                     </div>
-
                     <div class="field-wrap">
                         <label>
                             Password<span class="req"></span>
                         </label>
                         <input type="password" autocomplete="off" name='password' id="password"/>
                     </div>
-
                     <div class="field-wrap">
                         <label>
                             Confirm Password<span class="req"></span>
                         </label>
                         <input type="password" autocomplete="off" name='confirmPassword' id="confirmPassword"/>
                     </div>
-
                     <div class="field-wrap">
                         <label>
                             Phone Number<span class="req"></span>
---
+++ BTZApp/accountManagement.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Author: Brian Treichel, Ting Ting Lin, Ziming Qi
+ * Purpose: Conestoga College Winter 2018 Capstone Project
+ * Date created: 2018-04-08
+ * File name: index.php
+ */
