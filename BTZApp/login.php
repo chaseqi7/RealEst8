1393f11d60d8d47fb04bbc276bf07a2ef47e2a06
KatrinaTingTingLin <31769438+KatrinaTingTingLin@users.noreply.github.com>
2018-04-11 19:54:57 -0400

    update login and profile

--- BTZApp/login.php
+++ BTZApp/login.php
@@ -20,16 +20,19 @@ else { // User exists
     $user = $result->fetch_assoc();
 
     if ( password_verify($_POST['password'], $user['Password'])) {
-
-        $_SESSION['email'] = $user['email'];
-        $_SESSION['first_name'] = $user['first_name'];
-        $_SESSION['last_name'] = $user['last_name'];
-        $_SESSION['active'] = $user['active'];
+        $roleId = $user['Role'];
+        $getRole = $mysqli->query("SELECT * FROM Role WHERE RoleID='$roleId'");
+        $role = $getRole->fetch_assoc();
+        $_SESSION['email'] = $user['Email'];
+        $_SESSION['first_name'] = $user['FirstName'];
+        $_SESSION['last_name'] = $user['LastName'];
+        $_SESSION['role'] = $role['Description'];
+        $_SESSION['active'] = $user['Active'];
 
         // This is how we'll know the user is logged in
         $_SESSION['logged_in'] = true;
 
-        header("location: profile.php");
+        header("location: index.php");
     }
     else {
         $_SESSION['message'] = "You have entered wrong password, try again!";
--- BTZApp/profile.php
+++ BTZApp/profile.php
@@ -10,17 +10,20 @@
 session_start();
 
 // Check if user is logged in using the session variable
-if ( $_SESSION['logged_in'] != 1 ) {
+if ( !isset($_SESSION['logged_in']) || !$_SESSION['logged_in'] ) {
     $_SESSION['message'] = "You must log in before viewing your profile page!";
     header("location: error.php");
 }
 else {
     // Makes it easier to read
+    $email = $_SESSION['email'];
     $first_name = $_SESSION['first_name'];
     $last_name = $_SESSION['last_name'];
-    $email = $_SESSION['email'];
+    $role = $_SESSION['role'];
     $active = $_SESSION['active'];
 }
+
+$result = $mysqli->query("SELECT * FROM UserT WHERE Email='$email'");
 ?>
 <!DOCTYPE html>
 <html >
@@ -35,58 +38,79 @@ else {
 <div id="nav-bar">
     <ul class="navbar">
         <li class="navTitle"><a href="index.php">Real Est8</a></li>
-
-        <li id="account-dropdown" class="dropdown">
-            <?php if($_SESSION['logged_in'] == 1 ) : ?>
-                <a href="profile.php" class="dropbtn"><?php echo $first_name . $last_name ?></a>
-            <?php else : ?>
-                <a href="account.php" class="dropbtn">Sign in</a>
-            <?php endif; ?>
-        </li>
+        <?php
+        if (!isset($_SESSION['logged_in']) || !$_SESSION['logged_in']){
+            echo "<li id=\"account-dropdown\" class=\"dropdown\">";
+            echo "<a href=\"account.php\" class=\"dropbtn\">Sign in</a>";
+            echo "</li>";
+        }
+        else{
+            if (isset($_SESSION['role']) && $_SESSION['role'] === 'Admin'){
+                echo "<li class=\"dropdown\"><a href=\"accountManagement.php\">Manage Accounts</a></li>";
+            }
+            echo "<li id=\"account-dropdown\" class=\"dropdown\">";
+            echo "<a href=\"profile.php\" class=\"dropbtn\">Profile</a>";
+            echo "<div id=\"account-dropdown-content\" class=\"dropdown-content\">";
+            echo "<a href=\"logout.php\">Sign out</a>";
+            echo "</div>";
+            echo "</li>";
+        }
+        ?>
     </ul>
 </div>
-<!--Navbar-->
-<div class="form">
-
-    <h1>Welcome</h1>
-
+<div>
+    <h2>Welcome <?php echo $first_name.' '.$last_name; ?></h2>
     <p>
         <?php
-
         // Display message about account verification link only once
         if ( isset($_SESSION['message']) )
         {
             echo $_SESSION['message'];
-
             // Don't annoy the user with more messages upon page refresh
             unset( $_SESSION['message'] );
         }
-
         ?>
     </p>
-
     <?php
-
     // Keep reminding the user this account is not active, until they activate
     if ( !$active ){
         echo
         '<div class="info">
               Account is unverified, please confirm your email by clicking
               on the email link!
-              </div>';
+        </div>';
     }
-
     ?>
-
-    <h2><?php echo $first_name.' '.$last_name; ?></h2>
-    <p><?= $email ?></p>
-
-    <a href="logout.php"><button class="button button-block" name="logout"/>Log Out</button></a>
-
+    <table>
+        <?php
+        if ($result->num_rows > 0) {
+            // output data of each row
+            while($row = $result->fetch_assoc()) {
+                echo '<label>First name:</label>';
+                echo '<p>'.$row["FirstName"].'</p>'."<br>";
+                echo '<label>Last name:</label>';
+                echo '<p>'.$row["LastName"].'</p>'."<br>";
+                echo '<label>Address:</label>';
+                echo '<p>'.$row["Address"].'</p>'."<br>";
+                echo '<label>City:</label>';
+                echo '<p>'.$row["City"].'</p>'."<br>";
+                echo '<label>Province:</label>';
+                echo '<p>'.$row["Province"].'</p>'."<br>";
+                echo '<label>PostalCode:</label>';
+                echo '<p>'.$row["PostalCode"].'</p>'."<br>";
+                echo '<label>PhoneNumber:</label>';
+                echo '<p>'.$row["PhoneNumber"].'</p>'."<br>";
+            }
+        } else {
+            echo
+            '<div class="info">
+                  0 results
+                  </div>';
+        }
+        ?>
+    </table>
 </div>
-
 <script src='http://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>
 <script src="js/index.js"></script>
-
 </body>
 </html>
