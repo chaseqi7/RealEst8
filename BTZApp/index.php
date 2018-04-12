0e58af47888c6db3486bb9e1ae28878b8cff3d28
Chase Qi <qiziming1@gmail.com>
2018-04-12 16:48:15 -0400

    Show listings

--- BTZApp/index.php
+++ BTZApp/index.php
@@ -11,10 +11,18 @@ session_start();
 // Check if user is logged in using the session variable
 if ( isset($_SESSION['logged_in']) && $_SESSION['logged_in'] ) {
     $email = $_SESSION['email'];
-    $first_name = $_SESSION['firstname'];
-    $last_name = $_SESSION['lastname'];
+    $first_name = $_SESSION['first_name'];
+    $last_name = $_SESSION['last_name'];
     $role = $_SESSION['role'];
+    $active = $_SESSION['active'];
 }
+
+
+$servername = "localhost:3306";
+$username = "root";
+$password = "1234";
+$dbname = "BTZDatabase";
+$conn = new mysqli($servername, $username, $password, $dbname);
 ?>
 <!doctype html>
 <html class="no-js" lang="">
@@ -49,17 +57,12 @@ if ( isset($_SESSION['logged_in']) && $_SESSION['logged_in'] ) {
                 echo "<a href=\"account.php\" class=\"dropbtn\">Sign in</a>";
                 echo "</li>";
             }
-            else {
-                echo "<li id=\"account-dropdown\" class=\"dropdown\">";
-                echo "<a href=\"profile.php\" class=\"dropbtn\">Profile</a>";
-                echo "<div id=\"account-dropdown-content\" class=\"dropdown-content\">";
-                if (isset($_SESSION['role']) && $_SESSION['role'] === 'Admin'){
-                    echo "<a href=\"accountManagement.php\">Manage Accounts</a>";
-                }
-                echo "<a href=\"logout.php\">Sign out</a>";
-                echo "</div>";
-                echo "</li>";
-            }
+            echo "<li id=\"account-dropdown\" class=\"dropdown\">";
+            echo "<a href=\"profile.php\" class=\"dropbtn\">Profile</a>";
+            echo "<div id=\"account-dropdown-content\" class=\"dropdown-content\">";
+            echo "<a href=\"logout.php\">Sign out</a>";
+            echo "</div>";
+            echo "</li>";
             ?>
         </ul>
     </div>
@@ -68,6 +71,40 @@ if ( isset($_SESSION['logged_in']) && $_SESSION['logged_in'] ) {
         <input id="mainSearchBar" type="text" name="search" placeholder="Search..">
         <button id="searchButton" name="searchButton">Search</button>
         <!--<button onclick="filterShowHide()" id="filter-button">Filters</button>-->
+        <?php
+            if ($conn->connect_error) {
+                die("Connection failed: " . $conn->connect_error);
+            }
+            else{
+                $result = $conn->query("SELECT * FROM PropertyType ORDER BY PropertyTypeID");
+                echo "<select name='roleSelect'>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<option value='" . $row['RoleID'] . "'>".$row['Description']."</option>";
+                }
+                echo "</select>";
+                $result = $conn->query("SELECT * FROM BuildingType ORDER BY BuildingTypeID");
+                echo "<select name='roleSelect'>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<option value='" . $row['RoleID'] . "'>".$row['Description']."</option>";
+                }
+                echo "</select>";
+                $result = $conn->query("SELECT * FROM Ownership ORDER BY OwnershipID");
+                echo "<select name='roleSelect'>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<option value='" . $row['RoleID'] . "'>".$row['Description']."</option>";
+                }
+                echo "</select>";
+                $result = $conn->query("SELECT * FROM SaleOrRent ORDER BY SaleOrRentID");
+                echo "<select name='roleSelect'>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<option value='" . $row['RoleID'] . "'>".$row['Description']."</option>";
+                }
+                echo "</select>";
+                echo "<select name='roleSelect'>";
+
+            }
+        ?>
+
         <div id="filter-pane" style="display: none">
             <form>
                 <h3>Waiting for filters...</h3>
@@ -87,74 +124,41 @@ if ( isset($_SESSION['logged_in']) && $_SESSION['logged_in'] ) {
         </script>
         <!--Suggestiongs/Search Results-->
         <div id="main-listings">
-        <?php
-        $query = $_GET['query'];
-        // gets value sent over search form
-        $min_length = 3;
-        // you can set minimum length of the query if you want
-        if(strlen($query) >= $min_length) { // if query length is more or equal minimum length then
-            $query = htmlspecialchars($query);
-            // changes characters used in html to their equivalents, for example: < to &gt;
-            $query = $mysqli->escape_string($query);
-            // makes sure nobody uses SQL injection
-            $raw_results = $mysqli->query("SELECT * FROM property
-                WHERE (`Price` LIKE '%" . $query . "%') OR (`ListedDate` LIKE '%" . $query . "%') OR (`NumberOfBedrooms` LIKE '%" . $query . "%') OR (`NumberOfWashrooms` LIKE '%" . $query . "%') OR (`Address` LIKE '%" . $query . "%') OR (`City` LIKE '%" . $query . "%') OR (`Province` LIKE '%" . $query . "%') OR (`PostalCode` LIKE '%" . $query . "%')") or die(mysql_error());
-            // * means that it selects all fields, you can also write: `id`, `title`, `text`
-            // articles is the name of our table
-            // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-            // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-            // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-            if (mysql_num_rows($raw_results) > 0) { // if one or more rows are returned do following
-                while ($results = mysql_fetch_array($raw_results)) {
-                    // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-                    echo "<p><h3>" . $results['title'] . "</h3>" . $results['text'] . "</p>";
-                    // posts results gotten from database(title and text) you can also show id ($results['id'])
-                }
-            } else { // if there is no matching rows do following
-                echo "No results";
-            }
-        }
-        else{ // if there is no matching rows do following
-            echo "No results";
-        }
-        ?>
+            <div id="listings">
+                <?php
 
-                    <h2>Suggestions</h2><!--Should change 'Results' when someone searches for something-->
-                <div id="listings">
-                    <a href="listing-detail.html" class="listing-link">
-                        <div class="listing">
-                            <img class="listing-image" src="img/example-house.jpg" />
-                            <h3 class="listing-address">123 The Best Street</h3><br>
-                            <h3 class="listing-city">Kitchener ON</h3><br>
-                            <h4 class="listing-price">$150 000</h4><br>
-                            <div class="listing-extra-div1">
-                                <p class="listing-detail">Detail Label: </p>
-                                <p class="listing-detail">77 Rooms</p>
-                            </div>
-                            <div class="listing-extra-div2">
-                                <p class="listing-detail">Detail Label: </p>
-                                <p class="listing-detail">8 Bathrooms</p><br>
-                            </div>
-                        </div>
-                    </a>
-                    <a href="listing-detail.html" class="listing-link">
-                        <div class="listing">
-                            <img class="listing-image" src="img/example-house2.jpg" />
-                            <h3 class="listing-address">123 The Best Street</h3><br>
-                            <h3 class="listing-city">Kitchener ON</h3><br>
-                            <h4 class="listing-price">$150 000</h4><br>
-                            <div class="listing-extra-div1">
-                                <p class="listing-detail">Detail Label: </p>
-                                <p class="listing-detail">77 Rooms</p>
-                            </div>
-                            <div class="listing-extra-div2">
-                                <p class="listing-detail">Detail Label: </p>
-                                <p class="listing-detail">8 Bathrooms</p><br>
-                            </div>
-                        </div>
-                    </a>
-                </div>
+                    // Check connection
+                    if ($conn->connect_error) {
+                        die("Connection failed: " . $conn->connect_error);
+                    }
+                    else{
+                        $selectSQL = "SELECT * FROM Property";
+                        $result = $conn->query($selectSQL);
+                        while($row = $result->fetch_array())
+                        {
+                            echo' 
+                            <a href="listing-detail.html" class="listing-link">
+                                <div class="listing">
+                                    <img class="listing-image" src="img/example-house.jpg" />
+                                    <h3 class="listing-address">'.$row["Address"].'</h3><br>
+                                    <h3 class="listing-city">'.$row["City"]." ".$row["Province"].'</h3><br>
+                                    <h4 class="listing-price">$'.$row["Price"].'</h4><br>
+                                    <div class="listing-extra-div1">
+                                        <p class="listing-detail">Number Of Bedrooms: </p>
+                                        <p class="listing-detail">'.$row["NumberOfBedrooms"].'</p>
+                                    </div>
+                                    <div class="listing-extra-div2">
+                                        <p class="listing-detail">Number Of Bathrooms: </p>
+                                        <p class="listing-detail">'.$row["NumberOfWashrooms"].'</p><br>
+                                    </div>
+                                </div>
+                            </a>
+                        ';
+                        }
+                    }
+                ?>
             </div>
+        </div>
 
     <script src="js/vendor/modernizr-3.5.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
