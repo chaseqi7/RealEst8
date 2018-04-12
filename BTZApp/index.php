feec730b12d75bae1de7a37f85039aa5f809aaf4
KatrinaTingTingLin <31769438+KatrinaTingTingLin@users.noreply.github.com>
2018-04-12 10:21:13 -0400

    fix Brian merge conflict

--- BTZApp/index.php
+++ BTZApp/index.php
@@ -50,146 +50,107 @@ if ( isset($_SESSION['logged_in']) && $_SESSION['logged_in'] ) {
                 echo "<a href=\"account.php\" class=\"dropbtn\">Sign in</a>";
                 echo "</li>";
             }
-<<<<<<< HEAD
-<<<<<<< HEAD
             echo "<li id=\"account-dropdown\" class=\"dropdown\">";
             echo "<a href=\"profile.php\" class=\"dropbtn\">Profile</a>";
             echo "<div id=\"account-dropdown-content\" class=\"dropdown-content\">";
             echo "<a href=\"logout.php\">Sign out</a>";
             echo "</div>";
             echo "</li>";
-        }
-        ?>
-    </ul>
-</div>
-<!--Search Bar-->
-<div id="search-container" align="center">
-    <input id="mainSearchBar" type="text" name="search" placeholder="Search..">
-    <button id="searchButton" name="searchButton">Search</button>
-    <!--<button onclick="filterShowHide()" id="filter-button">Filters</button>-->
-    <div id="filter-pane" style="display: none">
-        <form>
-            <h3>Waiting for filters...</h3>
-        </form>
-=======
-=======
->>>>>>> a565058aee00f7e7d2a86696580c89171e3224a3
-            else{
-                if (isset($_SESSION['role']) && $_SESSION['role'] === 'Admin'){
-                    echo "<li class=\"dropdown\"><a href=\"accountManagement.php\">Manage Accounts</a></li>";
-                }
-                echo "<li id=\"account-dropdown\" class=\"dropdown\">";
-                echo "<a href=\"profile.php\" class=\"dropbtn\">Profile</a>";
-                echo "<div id=\"account-dropdown-content\" class=\"dropdown-content\">";
-                echo "<a href=\"logout.php\">Sign out</a>";
-                echo "</div>";
-                echo "</li>";
-            }
             ?>
         </ul>
-<<<<<<< HEAD
->>>>>>> a565058aee00f7e7d2a86696580c89171e3224a3
-=======
->>>>>>> a565058aee00f7e7d2a86696580c89171e3224a3
     </div>
     <!--Search Bar-->
     <div id="search-container" align="center">
         <input id="mainSearchBar" type="text" name="search" placeholder="Search..">
-        <button onclick="filterShowHide()" id="filter-button">Filters</button>
+        <button id="searchButton" name="searchButton">Search</button>
+        <!--<button onclick="filterShowHide()" id="filter-button">Filters</button>-->
         <div id="filter-pane" style="display: none">
             <form>
                 <h3>Waiting for filters...</h3>
             </form>
         </div>
-    </div>
-    <script>
-        /* When the user clicks on the button,
-        toggle between hiding and showing the dropdown content */
-        function filterShowHide() {
-            var x = document.getElementById("filter-pane");
-            if (x.style.display === "none") {
-                x.style.display = "block";
-            } else {
-                x.style.display = "none";
-<<<<<<< HEAD
-=======
+        <script>
+            /* When the user clicks on the button,
+            toggle between hiding and showing the dropdown content */
+            function filterShowHide() {
+                var x = document.getElementById("filter-pane");
+                if (x.style.display === "none") {
+                    x.style.display = "block";
+                } else {
+                    x.style.display = "none";
+                }
+            }
+        </script>
+        <!--Suggestiongs/Search Results-->
+        <div id="main-listings">
+        <?php
+        $query = $_GET['query'];
+        // gets value sent over search form
+        $min_length = 3;
+        // you can set minimum length of the query if you want
+        if(strlen($query) >= $min_length) { // if query length is more or equal minimum length then
+            $query = htmlspecialchars($query);
+            // changes characters used in html to their equivalents, for example: < to &gt;
+            $query = $mysqli->escape_string($query);
+            // makes sure nobody uses SQL injection
+            $raw_results = $mysqli->query("SELECT * FROM property
+                WHERE (`Price` LIKE '%" . $query . "%') OR (`ListedDate` LIKE '%" . $query . "%') OR (`NumberOfBedrooms` LIKE '%" . $query . "%') OR (`NumberOfWashrooms` LIKE '%" . $query . "%') OR (`Address` LIKE '%" . $query . "%') OR (`City` LIKE '%" . $query . "%') OR (`Province` LIKE '%" . $query . "%') OR (`PostalCode` LIKE '%" . $query . "%')") or die(mysql_error());
+            // * means that it selects all fields, you can also write: `id`, `title`, `text`
+            // articles is the name of our table
+            // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
+            // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
+            // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
+            if (mysql_num_rows($raw_results) > 0) { // if one or more rows are returned do following
+                while ($results = mysql_fetch_array($raw_results)) {
+                    // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
+                    echo "<p><h3>" . $results['title'] . "</h3>" . $results['text'] . "</p>";
+                    // posts results gotten from database(title and text) you can also show id ($results['id'])
+                }
+            } else { // if there is no matching rows do following
+                echo "No results";
             }
         }
-    </script>
-    <!--Search Bar-->
-    <!--Suggestiongs/Search Results-->
-    <div id="main-listings">
-    <?php
-    $query = $_GET['query'];
-    // gets value sent over search form
-    $min_length = 3;
-    // you can set minimum length of the query if you want
-    if(strlen($query) >= $min_length){ // if query length is more or equal minimum length then
-        $query = htmlspecialchars($query);
-        // changes characters used in html to their equivalents, for example: < to &gt;
-        $query = $mysqli->escape_string($query);
-        // makes sure nobody uses SQL injection
-        $raw_results = $mysqli->query("SELECT * FROM property
-            WHERE (`Price` LIKE '%".$query."%') OR (`ListedDate` LIKE '%".$query."%') OR (`NumberOfBedrooms` LIKE '%".$query."%') OR (`NumberOfWashrooms` LIKE '%".$query."%') OR (`Address` LIKE '%".$query."%') OR (`City` LIKE '%".$query."%') OR (`Province` LIKE '%".$query."%') OR (`PostalCode` LIKE '%".$query."%')") or die(mysql_error());
-        // * means that it selects all fields, you can also write: `id`, `title`, `text`
-        // articles is the name of our table
-        // '%$query%' is what we're looking for, % means anything, for example if $query is Hello
-        // it will match "hello", "Hello man", "gogohello", if you want exact match use `title`='$query'
-        // or if you want to match just full word so "gogohello" is out use '% $query %' ...OR ... '$query %' ... OR ... '% $query'
-        if(mysql_num_rows($raw_results) > 0){ // if one or more rows are returned do following
-            while($results = mysql_fetch_array($raw_results)){
-                // $results = mysql_fetch_array($raw_results) puts data from database into array, while it's valid it does the loop
-                echo "<p><h3>".$results['title']."</h3>".$results['text']."</p>";
-                // posts results gotten from database(title and text) you can also show id ($results['id'])
->>>>>>> a565058aee00f7e7d2a86696580c89171e3224a3
-            }
+        else{ // if there is no matching rows do following
+            echo "No results";
         }
-    </script>
-    <!--Search Bar-->
-    <!--Suggestiongs/Search Results-->
-    <div id="main-listings">
-    <?php
+        ?>
 
-    ?>
-    <!--            <h2>Suggestions</h2><!--Should change 'Results' when someone searches for something-->
-    <!--        <div id="listings">-->
-    <!--            <a href="listing-detail.html" class="listing-link">-->
-    <!--                <div class="listing">-->
-    <!--                    <img class="listing-image" src="img/example-house.jpg" />-->
-    <!--                    <h3 class="listing-address">123 The Best Street</h3><br>-->
-    <!--                    <h3 class="listing-city">Kitchener ON</h3><br>-->
-    <!--                    <h4 class="listing-price">$150 000</h4><br>-->
-    <!--                    <div class="listing-extra-div1">-->
-    <!--                        <p class="listing-detail">Detail Label: </p>-->
-    <!--                        <p class="listing-detail">77 Rooms</p>-->
-    <!--                    </div>-->
-    <!--                    <div class="listing-extra-div2">-->
-    <!--                        <p class="listing-detail">Detail Label: </p>-->
-    <!--                        <p class="listing-detail">8 Bathrooms</p><br>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </a>-->
-    <!--            <a href="listing-detail.html" class="listing-link">-->
-    <!--                <div class="listing">-->
-    <!--                    <img class="listing-image" src="img/example-house2.jpg" />-->
-    <!--                    <h3 class="listing-address">123 The Best Street</h3><br>-->
-    <!--                    <h3 class="listing-city">Kitchener ON</h3><br>-->
-    <!--                    <h4 class="listing-price">$150 000</h4><br>-->
-    <!--                    <div class="listing-extra-div1">-->
-    <!--                        <p class="listing-detail">Detail Label: </p>-->
-    <!--                        <p class="listing-detail">77 Rooms</p>-->
-    <!--                    </div>-->
-    <!--                    <div class="listing-extra-div2">-->
-    <!--                        <p class="listing-detail">Detail Label: </p>-->
-    <!--                        <p class="listing-detail">8 Bathrooms</p><br>-->
-    <!--                    </div>-->
-    <!--                </div>-->
-    <!--            </a>-->
-    <!--        </div>-->
-    <!--    </div>-->
-
-    <!--Suggestiongs/Search Results-->
-
+        <!--            <h2>Suggestions</h2><!--Should change 'Results' when someone searches for something-->
+        <!--        <div id="listings">-->
+        <!--            <a href="listing-detail.html" class="listing-link">-->
+        <!--                <div class="listing">-->
+        <!--                    <img class="listing-image" src="img/example-house.jpg" />-->
+        <!--                    <h3 class="listing-address">123 The Best Street</h3><br>-->
+        <!--                    <h3 class="listing-city">Kitchener ON</h3><br>-->
+        <!--                    <h4 class="listing-price">$150 000</h4><br>-->
+        <!--                    <div class="listing-extra-div1">-->
+        <!--                        <p class="listing-detail">Detail Label: </p>-->
+        <!--                        <p class="listing-detail">77 Rooms</p>-->
+        <!--                    </div>-->
+        <!--                    <div class="listing-extra-div2">-->
+        <!--                        <p class="listing-detail">Detail Label: </p>-->
+        <!--                        <p class="listing-detail">8 Bathrooms</p><br>-->
+        <!--                    </div>-->
+        <!--                </div>-->
+        <!--            </a>-->
+        <!--            <a href="listing-detail.html" class="listing-link">-->
+        <!--                <div class="listing">-->
+        <!--                    <img class="listing-image" src="img/example-house2.jpg" />-->
+        <!--                    <h3 class="listing-address">123 The Best Street</h3><br>-->
+        <!--                    <h3 class="listing-city">Kitchener ON</h3><br>-->
+        <!--                    <h4 class="listing-price">$150 000</h4><br>-->
+        <!--                    <div class="listing-extra-div1">-->
+        <!--                        <p class="listing-detail">Detail Label: </p>-->
+        <!--                        <p class="listing-detail">77 Rooms</p>-->
+        <!--                    </div>-->
+        <!--                    <div class="listing-extra-div2">-->
+        <!--                        <p class="listing-detail">Detail Label: </p>-->
+        <!--                        <p class="listing-detail">8 Bathrooms</p><br>-->
+        <!--                    </div>-->
+        <!--                </div>-->
+        <!--            </a>-->
+        <!--        </div>-->
+        <!--    </div>-->
 
     <script src="js/vendor/modernizr-3.5.0.min.js"></script>
     <script>window.jQuery || document.write('<script src="js/vendor/jquery-3.2.1.min.js"><\/script>')</script>
